a1b8eec329648d3ebb406eef50bde0bbdd0f10a0
tundergod <tundergod1882@gmail.com>
2017-10-02 14:16:14 +0800

    personal website done v1

---
+++ js/showtext.js
@@ -0,0 +1,6 @@
+var showtext = function (target, message, index, interval) {   
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
+}
